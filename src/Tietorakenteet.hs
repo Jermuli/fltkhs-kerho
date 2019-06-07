83497705ec2cccb5f11e04a79d1a99e4844c901b
Jere Pakkanen <jejopakk@student.jyu.fi>
2019-06-07 18:44:30 +0300

    Täydennetty tietorakenteita

--- src/Tietorakenteet.hs
+++ src/Tietorakenteet.hs
@@ -1,5 +1,20 @@
 module Tietorakenteet where
 
---TODO: tee tietorakenteet
-data Jasen = Jasen { nimi ::String
-                   , puhelinumero :: Int} deriving (Show)
+--TODO: lisää tietorakenteita muokkaavat ja käsittelevät funktiot sekä relaatiot
+data Jasen = Jasen { nimi :: String
+                   , hetu :: String
+                   , katuosoite :: String
+                   , postinumero :: Int
+                   , kotipuhelin :: Int
+                   , tyopuhelin :: Int
+                   , autopuhelin :: Int
+                   , liittymisvuosi :: Int
+                   , jasenmaksu :: Double
+                   , maksettu :: Double
+                   , lisatieto :: String} deriving (Show)
+
+data Harrastus = Harrastus { laji :: String
+                           , aloitusvuosi :: Int
+                           , tuntiaViikossa :: Double} deriving (Show)
+                            
+type Kerho = [Jasen]
