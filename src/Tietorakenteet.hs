47ba5e2a123abd6a765f594319187066f9f089d3
Jere Pakkanen <jejopakk@student.jyu.fi>
2019-08-03 18:10:59 +0300

    korjattu tyyppivirheitä

--- src/Tietorakenteet.hs
+++ src/Tietorakenteet.hs
@@ -1,4 +1,5 @@
 module Tietorakenteet where
+import Data.List
 import qualified Data.Text as T
 
 --TODO: lisää tietorakenteita muokkaavat ja käsittelevät funktiot sekä relaatiot, sekä selvitä onko relaatiot edes tarpeellisia
@@ -15,11 +16,11 @@ data Jasen = Jasen { nimi :: Maybe T.Text
                    , jasenmaksu :: Maybe Double
                    , maksettu :: Maybe Double
                    , lisatieto :: Maybe T.Text
-                   , harrastukset :: [Harrastus]} deriving (Show)
+                   , harrastukset :: [Harrastus]} deriving (Eq, Show)
 
 data Harrastus = Harrastus { laji :: Maybe T.Text
                            , aloitusvuosi :: Maybe Int
-                           , tuntiaViikossa :: Maybe Double} deriving (Show)
+                           , tuntiaViikossa :: Maybe Double} deriving (Eq, Show)
                             
 data Kerho = Kerho { kerhonNimi :: Maybe T.Text
                    , jasenet :: [Jasen]}
@@ -35,20 +36,20 @@ lisaajasen :: Jasen -> Kerho -> Kerho
 lisaajasen uusijasen kerho = Kerho (kerhonNimi kerho) ((jasenet kerho) ++ [uusijasen])
 
 poistaJasen :: Jasen -> [Jasen] -> [Jasen]
-poistaJasen poistettava jasenet = case kerho of
+poistaJasen poistettava jasenet = case jasenet of
                                     []  -> []
                                     x:xs   -> case x == poistettava of
                                                 True    -> xs
-                                                False   -> x:(poistaJasen xs)
+                                                False   -> x:(poistaJasen poistettava xs)
 
 muokkaaJasen :: Jasen -> Jasen -> [Jasen] -> [Jasen]
 muokkaaJasen muokattava uudettiedot jasenet = case jasenet of
                                                 []  ->  []
                                                 x:xs -> case x == muokattava of
                                                             True    -> uudettiedot:xs
-                                                            False   -> x:(muokkaaJasen xs)
+                                                            False   -> x:(muokkaaJasen muokattava uudettiedot xs)
 lisaaHarrastus :: Harrastus -> Jasen -> Jasen
-lisaaHarrastus harrastus jasen = Jasen (nimi jasen) (hetu jasen) (katuosoite jasen) (postinumero jasen) (postiosoite jasen) (kotipuhelin jasen) (tyopuhelin jasen) (autopuhelin jasen) (liittymisvuosi jasen) (jasenmaksu jasen) (maksettu jasen) (lisatieto jasen) ((harrastukset jasen) ++ harrastus)
+lisaaHarrastus harrastus jasen = Jasen (nimi jasen) (hetu jasen) (katuosoite jasen) (postinumero jasen) (postiosoite jasen) (kotipuhelin jasen) (tyopuhelin jasen) (autopuhelin jasen) (liittymisvuosi jasen) (jasenmaksu jasen) (maksettu jasen) (lisatieto jasen) ((harrastukset jasen) ++ [harrastus])
 
 poistaHarrastus :: Harrastus -> Jasen -> Jasen
 poistaHarrastus poistettava jasen = Jasen (nimi jasen) (hetu jasen) (katuosoite jasen) (postinumero jasen) (postiosoite jasen) (kotipuhelin jasen) (tyopuhelin jasen) (autopuhelin jasen) (liittymisvuosi jasen) (jasenmaksu jasen) (maksettu jasen) (lisatieto jasen) (delete poistettava (harrastukset jasen))
